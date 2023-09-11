9db4a1322b53e076cb538323d864775d3beebe4a
Jonas Hemesath <hemesath@neuro.mpg.de>
2023-09-11 15:05:59 +0200

    blender script done and offset tiling wip

--- blender.py
+++ blender.py
@@ -1,32 +1,18 @@
 import bpy
-import numpy as np
 import json
 
-beamline = 'ANATOMIX_SOLEIL'
-voxel_size = '650nm'
+path = 'C:\\Users\\hemesath\\python_experiments\\CT_positions\\'
 
-name = beamline + '_' + voxel_size
-p = 'J:\\Jonas Hemesath\\Synchrotron_time_calculations\\220112\\' + name + '\\'
-
-volume = np.load('C:\\Users\\hemesath\\Desktop\\' + name + '.npy')
-
-with open(p + name + '.json', 'r') as f:
+with open(path + 'blender_data.json', 'r') as f:
     data = json.load(f)
 
-volume = np.load(p + name + '.npy')
+bpy.ops.import_mesh.stl(filepath=path + "overview.stl")
+brain = bpy.context.selected_objects[0]
+brain.location = (data['mesh_origin'][1], data['mesh_origin'][0], data['mesh_origin'][2])
 
-#for x in range(1):
-for x in range(volume.shape[1]):
-    x1 = np.mean([x * data[0] * data[2], (x+1) * data[0] * data[2]])
+for p in data['positions']:
 
-    for y in range(volume.shape[2]):
-        y1 = np.mean([y * data[0] * data[2], (y+1) * data[0] * data[2]])
-
-        for z in range(volume.shape[0]):
-            z1 = np.mean([z * data[1] * data[2], (z+1) * data[1] * data[2]])
-
-            if volume[z, x, y] > 0:
-                bpy.ops.mesh.primitive_cylinder_add()
-                cylinder = bpy.context.selected_objects[0]
-                cylinder.location = (x1, y1, z1)
-                cylinder.scale = (data[3]/2, data[3]/2, data[4]/2)
+    bpy.ops.mesh.primitive_cylinder_add()
+    cylinder = bpy.context.selected_objects[0]
+    cylinder.location = (p[1], p[0], p[2])
+    cylinder.scale = (data['tilesize'][0]/2, data['tilesize'][0]/2, data['tilesize'][1]/2)
--- calculate_positions.py
+++ calculate_positions.py
@@ -6,10 +6,11 @@ import numpy as np
 import math
 import trimesh
 from skimage import measure
+import json
 
 
 # Parameters
-fp_overview = 'J:\\Jonas Hemesath\\Synchrotron_time_calculations\\220112\\bw_images_rot'        # images have to be binary
+fp_overview = 'J:\\Jonas Hemesath\\Synchrotron_time_calculations\\220112\\bw_images'        # images have to be binary
 overview_pos = (0, 0, 0)            # x, y, z in micrometer
 overview_voxel_size = 24.41         # in micrometer
 
@@ -28,14 +29,16 @@ time_per_tomogram = 12              # in minutes
 overlap_between_tomograms = 0.31    # fraction
 overlap_column = 100                # tile voxel
 
-tomogram_overlap = 'linear'         # equals to either 'linear' or 'offset'
+tomogram_overlap = 'offset'         # equals to either 'linear' or 'offset'
 
 export_brain_mesh = True
 downsample = 2
 
+origin_offset = 5                   # mm from the lower left corner of the overview
+
 
 def calculate_postions(fp_overview, overview_pos, overview_voxel_size, fp_output, detector_px_x, detector_px_y, half_aquisition, half_acquistion_overlap,
-                       hres_voxel_size, time_per_tomogram, overlap_between_tomograms, overlap_column, tomogram_overlap):
+                       hres_voxel_size, time_per_tomogram, overlap_between_tomograms, overlap_column, tomogram_overlap, origin_offset):
     
     # Overview
     overview_paths = list(Path(fp_overview).iterdir())
@@ -48,7 +51,8 @@ def calculate_postions(fp_overview, overview_pos, overview_voxel_size, fp_output
     overview_size_xy_um = overview_voxel_size * overview.shape[0]
     overview_size_z_um = overview_voxel_size * overview.shape[2]
 
-    top_left = (overview_pos[0]-overview_size_xy_um/2, overview_pos[1]+overview_size_xy_um/2, overview_pos[2]+overview_size_z_um/2)
+    top_left = (overview_pos[0]+overview_size_xy_um/2, overview_pos[1]-overview_size_xy_um/2, overview_pos[2]+overview_size_z_um/2)
+    lower_left = ((top_left[0]-overview_size_xy_um)*0.001, top_left[1]*0.001, (top_left[2]-overview_size_z_um)*0.001)
 
     # Tile 
     if half_aquisition:
@@ -69,40 +73,99 @@ def calculate_postions(fp_overview, overview_pos, overview_voxel_size, fp_output
     i_y = math.ceil(overview.shape[1]/ts_xy)
     i_z = math.ceil(overview.shape[2]/ts_z)
 
-    postions = []
+    positions = []
 
-    for x in range(i_x):
-        x1 = x * ts_xy
-        x2 = (x+1) * ts_xy
-        if x2 > overview.shape[0]:
-            x2 = -1
-            
-        for y in range(i_y):
-            y1 = y * ts_xy
-            y2 = (y+1) * ts_xy 
-            if y2 > overview.shape[0]:
-                y2 = -1
-            
-            for z in range(i_z):
-                z1 = z * ts_z
-                z2 = (z+1) * ts_z
-                if z2 > overview.shape[0]:
-                    z2 = -1
+    if tomogram_overlap == 'linear':
+        for x in range(i_x):
+            x1 = x * ts_xy
+            x2 = (x+1) * ts_xy
+            x3 = x2
+            if x2 > overview.shape[0]:
+                x2 = -1
+                
+            for y in range(i_y):
+                y1 = y * ts_xy
+                y2 = (y+1) * ts_xy 
+                y3 = y2
+                if y2 > overview.shape[0]:
+                    y2 = -1
+                
+                for z in range(i_z):
+                    z1 = z * ts_z
+                    z2 = (z+1) * ts_z
+                    z3 = z2
+                    if z2 > overview.shape[0]:
+                        z2 = -1
 
-                if np.any(overview[x1:x2, y1:y2, z1:z2] > 0):
-                    postions.append((top_left[0]*0.001 + np.mean([x2,x1])*overview_voxel_size*0.001, 
-                                     top_left[1]*0.001 - np.mean([y2,y1])*overview_voxel_size*0.001,
-                                     top_left[2]*0.001 - np.mean([z2,z1])*overview_voxel_size*0.001))
-                    
-    print('Number of tomograms:', len(postions))
-    print('Total scan time [hrs]:', len(postions)*time_per_tomogram/60)
+                    if np.any(overview[x1:x2, y1:y2, z1:z2] > 0):
+                        positions.append((top_left[0]*0.001 - np.mean([x3,x1])*overview_voxel_size*0.001, 
+                                        top_left[1]*0.001 + np.mean([y3,y1])*overview_voxel_size*0.001,
+                                        top_left[2]*0.001 - np.mean([z3,z1])*overview_voxel_size*0.001))
+                        
+    elif tomogram_overlap == 'offset':
+        for x in range(i_x):
+            x1 = x * ts_xy
+            x2 = (x+1) * ts_xy
+            x3 = x1
+            x4 = x2
+            if x2 > overview.shape[0]:
+                x2 = -1
+
+            if x%2 == 1:
+                i_y += 1
+                y_offset = ts_xy//2
+            else:
+                i_y -= 1
+                y_offset = 0
+                
+            for y in range(i_y):
+                y1 = y * ts_xy - y_offset
+                y2 = (y+1) * ts_xy - y_offset
+                y3 = y1
+                y4 = y2
+                if y1 < 0:
+                    y1 = 0
+                if y2 > overview.shape[0]:
+                    y2 = -1
+                
+                for z in range(i_z):
+                    z1 = z * ts_z
+                    z2 = (z+1) * ts_z
+                    z3 = z1
+                    z4 = z2
+                    if z2 > overview.shape[0]:
+                        z2 = -1
+
+                    if np.any(overview[x1:x2, y1:y2, z1:z2] > 0):
+                        positions.append((top_left[0]*0.001 - np.mean([x4,x3])*overview_voxel_size*0.001, 
+                                        top_left[1]*0.001 + np.mean([y4,y3])*overview_voxel_size*0.001,
+                                        top_left[2]*0.001 - np.mean([z4,z3])*overview_voxel_size*0.001))
+
+    # Determine origin and normalize to origin
+    origin = [lower_left[i]-origin_offset for i in range(3)]
+
+    mesh_origin = [lower_left[i] - origin[i] for i in range(3)]
+
+    norm_positions = [[p[i] - origin[i] for i in range(3)] for p in positions]
+
+    # Exports
+
+    print('Number of tomograms:', len(positions))
+    print('Total scan time [hrs]:', len(positions)*time_per_tomogram/60)
 
     with open(fp_output + 'postions.txt', 'w') as f:
-        for p in postions:
-            line = 'uvm(sx, ' + str(p[0]) + ', sy, ' + str(p[1]) + ', sz, ' + str(p[2]) + ')\n'
+        for p in positions:
+            line = 'uvm(sx, ' + str(p[0]-origin[0]) + ', sy, ' + str(p[1]-origin[1]) + ', sz, ' + str(p[2]-origin[2]) + ')\n'
             f.write(line)
+
+
+    blender_data = {'origin': origin, 'mesh_origin': mesh_origin, 'positions': norm_positions, 'tilesize': (tile_size_xy_um*0.001, tile_size_z_um*0.001)}
+
+    with open('blender_data.json', 'w') as f:
+        json.dump(blender_data, f)
+
                     
-    return postions
+    return positions
 
 
 def output_brain_mesh(fp_overview, fp_output, overview_voxel_size, downsample=None):
@@ -111,7 +174,8 @@ def output_brain_mesh(fp_overview, fp_output, overview_voxel_size, downsample=No
 
     overview = np.rot90(overview, 1, (0,1))
     overview = np.rot90(overview, 1, (1,2))
-    overview = np.rot90(overview, 2, (0,1))
+    overview = np.rot90(overview, 1, (0,1))
+    overview = np.flip(overview, 2)
     print('Overview loaded')
     vx = overview_voxel_size * 0.001
     if downsample:
@@ -134,7 +198,7 @@ def output_brain_mesh(fp_overview, fp_output, overview_voxel_size, downsample=No
 
 if __name__ == '__main__':
     positions = calculate_postions(fp_overview, overview_pos, overview_voxel_size, fp_output, detector_px_x, detector_px_y, half_aquisition, half_acquistion_overlap,
-                       hres_voxel_size, time_per_tomogram, overlap_between_tomograms, overlap_column, tomogram_overlap)
+                       hres_voxel_size, time_per_tomogram, overlap_between_tomograms, overlap_column, tomogram_overlap, origin_offset)
     
     if export_brain_mesh:
         output_brain_mesh(fp_overview, fp_output, overview_voxel_size, downsample)
